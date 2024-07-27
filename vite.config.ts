862e5ad25b098f195970f1bb80044e967d6ece63
Hirokazu Egashira <sekaibyoudou0712@gmail.com>
2024-07-27 11:58:33 +0900

    Delete comment out

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import glsl from 'vite-plugin-glsl';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-//const base = process.env.NODE_ENV === 'production' ? '/opencv/binary/' : '/';
-
 // __dirnameの代わりにfileURLToPathとimport.meta.urlを使用
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,10 +13,6 @@ export default defineConfig({
     build: {
         outDir: 'dist', // 出力ディレクトリを指定
     },
-    // define: {
-    //     'global.Module': 'window.Module',
-    //     'process.env': {}
-    // }
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
@@ -26,9 +20,7 @@ export default defineConfig({
     },
     css: {
         preprocessorOptions: {
-            scss: {
-                //additionalData: `@import "@/scss/style.scss";`
-            }
+            scss: {}
         }
     },
     publicDir: 'assets' // assetsフォルダを公開
